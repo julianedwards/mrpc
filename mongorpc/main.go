9c7beb90d9fc6d3c1bdbba4657d8f113a63fa6bc
Lydia Stepanek <lydia.stepanek@mongodb.com>
2019-07-16 10:46:52 -0400

    finish first handler

--- mongorpc/main.go
+++ mongorpc/main.go
@@ -36,25 +36,15 @@ func main() {
     }, func(ctx context.Context, w io.Writer, msg mongowire.Message) {
 				requestHeader := msg.Header()
         _, _ = pp.Print(requestHeader)
-				isMaster := bsonx.EC.Boolean("isMaster", true)
+				isMaster := bsonx.EC.Int32("ok", 1)
 
 				doc := bsonx.NewDocument(isMaster)
 				docBytes, err := doc.MarshalBSON()
 				docSimple := bson.Simple{BSON: docBytes, Size: int32(len(docBytes))}
         _, _ = pp.Print(err)
 
-				metadata := bsonx.NewDocument()
-				metadataBytes, err := metadata.MarshalBSON()
-				metadataSimple := bson.Simple{BSON: metadataBytes, Size: int32(len(metadataBytes))}
-        _, _ = pp.Print(err)
-
-				// outputDocs := bsonx.NewDocument()
-				// outputDocsBytes, err := doc.MarshalBSON()
-				// outputDocsSimple := bson.Simple{BSON: docBytes, Size: int32(len(outputDocsBytes))}
-
-				responseHeader := mongowire.MessageHeader{ResponseTo: requestHeader.RequestID, RequestID: 324, OpCode: mongowire.OP_COMMAND_REPLY}
-				commandReplyMessage := mongowire.CommandReplyMessage{CommandReplyHeader: responseHeader, CommandReply: docSimple, Metadata: metadataSimple}
-				w.Write(commandReplyMessage.Serialize())
+				newReply := mongowire.NewReply(0, 0, 0, 1, []bson.Simple{docSimple}) 
+				w.Write(newReply.Serialize())
     }); err != nil {
         grip.Error(err)
         return
