9b25f3464e2f8b435246320aba7bd9959027201e
Kim Tao <kim.tao@mongodb.com>
2019-07-16 13:48:44 -0400

    Add most of the required commands to start connection with mongo shell

--- mongorpc/main.go
+++ mongorpc/main.go
@@ -1,73 +1,148 @@
 package main
 
 import (
-    "context"
-    "io"
+	"context"
+	"io"
 
-    "github.com/k0kubun/pp"
-    "github.com/mongodb/grip"
-    "github.com/tychoish/mongorpc"
-    "github.com/tychoish/mongorpc/mongowire"
-    "github.com/tychoish/mongorpc/bson"
-		"github.com/mongodb/ftdc/bsonx"
+	"github.com/mongodb/ftdc/bsonx"
+	"github.com/mongodb/grip"
+	"github.com/pkg/errors"
+	"github.com/tychoish/mongorpc"
+	"github.com/tychoish/mongorpc/bson"
+	"github.com/tychoish/mongorpc/mongowire"
 )
 
+func handleIsMaster(ctx context.Context, w io.Writer, msg mongowire.Message) {
+	ok := bsonx.EC.Int32("ok", 1)
+	doc := bsonx.NewDocument(ok)
+	grip.Error(errors.Wrap(writeReply(doc, w), "could not make response to ismaster"))
+}
+
+func handleBuildInfo(ctx context.Context, w io.Writer, msg mongowire.Message) {
+	version := bsonx.EC.String("version", "0.0.0")
+	doc := bsonx.NewDocument(version)
+	grip.Error(errors.Wrap(writeReply(doc, w), "could not make response to buildinfo"))
+}
+
+func handleGetLog(ctx context.Context, w io.Writer, msg mongowire.Message) {
+	logs := bsonx.EC.String("logs", "hello")
+	doc := bsonx.NewDocument(logs)
+	grip.Error(errors.Wrap(writeReply(doc, w), "could not make response to getLog"))
+}
+
+func handleGetFreeMonitoringStatus(ctx context.Context, w io.Writer, msg mongowire.Message) {
+	ok := bsonx.EC.Int32("ok", 0)
+	doc := bsonx.NewDocument(ok)
+	grip.Error(errors.Wrap(writeReply(doc, w), "could not make response to getFreeMonitoringStatus"))
+}
+
+func writeReply(doc *bsonx.Document, w io.Writer) error {
+	resp, err := doc.MarshalBSON()
+	if err != nil {
+		return errors.Wrap(err, "problem marshalling response")
+	}
+	respDoc := bson.Simple{BSON: resp, Size: int32(len(resp))}
+
+	reply := mongowire.NewReply(0, 0, 0, 1, []bson.Simple{respDoc})
+	_, err = w.Write(reply.Serialize())
+	return errors.Wrap(err, "could not write response")
+}
+
 func main() {
-    ctx, cancel := context.WithCancel(context.Background())
-    defer cancel()
-    srv := mongorpc.NewService("localhost", 12345)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	srv := mongorpc.NewService("localhost", 12345)
 
-    // db.runCommand({whatever})
-    if err := srv.RegisterOperation(&mongowire.OpScope{
-        Type:    mongowire.OP_QUERY,
-        Context: "test.$cmd",
-    }, func(ctx context.Context, w io.Writer, msg mongowire.Message) {
-    }); err != nil {
-        grip.Error(err)
-        return
-    }
+	// db.runCommand({whatever})
+	if err := srv.RegisterOperation(&mongowire.OpScope{
+		Type:    mongowire.OP_QUERY,
+		Context: "test.$cmd",
+	}, func(ctx context.Context, w io.Writer, msg mongowire.Message) {
+	}); err != nil {
+		grip.Error(err)
+		return
+	}
 
-    // db.isMaster()
-    if err := srv.RegisterOperation(&mongowire.OpScope{
-        Type:    mongowire.OP_COMMAND,
-        Context: "admin",
-        Command: "isMaster",
-    }, func(ctx context.Context, w io.Writer, msg mongowire.Message) {
-				// requestHeader := msg.Header()
-				isOk := bsonx.EC.Int32("ok", 1)
+	// db.runCommand({isMaster: 1})
+	if err := srv.RegisterOperation(&mongowire.OpScope{
+		Type:    mongowire.OP_COMMAND,
+		Context: "admin",
+		Command: "isMaster",
+	}, handleIsMaster); err != nil {
+		grip.Error(errors.Wrap(err, "could not register handler for isMaster"))
+		return
+	}
 
-				doc := bsonx.NewDocument(isOk)
-				docBytes, err := doc.MarshalBSON()
-				docSimple := bson.Simple{BSON: docBytes, Size: int32(len(docBytes))}
-        _, _ = pp.Print(err)
+	if err := srv.RegisterOperation(&mongowire.OpScope{
+		Type:    mongowire.OP_COMMAND,
+		Context: "test",
+		Command: "isMaster",
+	}, handleIsMaster); err != nil {
+		grip.Error(errors.Wrap(err, "could not register handler for isMaster"))
+		return
+	}
 
-				newReply := mongowire.NewReply(0, 0, 0, 1, []bson.Simple{docSimple}) 
-				w.Write(newReply.Serialize())
-    }); err != nil {
-        grip.Error(err)
-        return
-    }
+	// db.runCommand({whatsmyuri: 1})
+	if err := srv.RegisterOperation(&mongowire.OpScope{
+		Type:    mongowire.OP_COMMAND,
+		Context: "admin",
+		Command: "whatsmyuri",
+	}, func(ctx context.Context, w io.Writer, msg mongowire.Message) {
+		uri := bsonx.EC.String("you", "localhost:12345")
+		doc := bsonx.NewDocument(uri)
+		grip.Error(errors.Wrap(writeReply(doc, w), "could not make response to whatsmyuri"))
+	}); err != nil {
+		grip.Error(errors.Wrap(err, "could not register handler for whatsmyuri"))
+		return
+	}
 
-    // db.whatsmyuri()
-    if err := srv.RegisterOperation(&mongowire.OpScope{
-        Type:    mongowire.OP_COMMAND,
-        Context: "admin",
-        Command: "whatsmyuri",
-    }, func(ctx context.Context, w io.Writer, msg mongowire.Message) {
-				// requestHeader := msg.Header()
-				myUri := bsonx.EC.String("you", "localhost:12345")
+	// db.runCommand({buildinfo: 1})
+	if err := srv.RegisterOperation(&mongowire.OpScope{
+		Type:    mongowire.OP_COMMAND,
+		Context: "admin",
+		Command: "buildinfo",
+	}, handleBuildInfo); err != nil {
+		grip.Error(errors.Wrap(err, "could not register handler for buildinfo"))
+		return
+	}
 
-				doc := bsonx.NewDocument(myUri)
-				docBytes, err := doc.MarshalBSON()
-				docSimple := bson.Simple{BSON: docBytes, Size: int32(len(docBytes))}
-        _, _ = pp.Print(err)
+	if err := srv.RegisterOperation(&mongowire.OpScope{
+		Type:    mongowire.OP_COMMAND,
+		Context: "test",
+		Command: "buildInfo",
+	}, handleBuildInfo); err != nil {
+		grip.Error(errors.Wrap(err, "could not register handler for buildinfo"))
+		return
+	}
 
-				newReply := mongowire.NewReply(0, 0, 0, 1, []bson.Simple{docSimple}) 
-				w.Write(newReply.Serialize())
-    }); err != nil {
-        grip.Error(err)
-        return
-    }
+	// db.runCommand({getLog: 1})
+	if err := srv.RegisterOperation(&mongowire.OpScope{
+		Type:    mongowire.OP_COMMAND,
+		Context: "admin",
+		Command: "getLog",
+	}, handleGetLog); err != nil {
+		grip.Error(errors.Wrap(err, "could not register handler for getLog"))
+		return
+	}
 
-    grip.Error(srv.Run(ctx))
+	if err := srv.RegisterOperation(&mongowire.OpScope{
+		Type:    mongowire.OP_COMMAND,
+		Context: "test",
+		Command: "getLog",
+	}, handleGetLog); err != nil {
+		grip.Error(errors.Wrap(err, "could not register handler for getLog"))
+		return
+	}
+
+	// db.runCommand({getFreeMonitoringStatus: 1})
+	if err := srv.RegisterOperation(&mongowire.OpScope{
+		Type:    mongowire.OP_COMMAND,
+		Context: "admin",
+		Command: "getFreeMonitoringStatus",
+	}, handleGetFreeMonitoringStatus); err != nil {
+		grip.Error(errors.Wrap(err, "could not register handler for getFreeMonitoringStatus"))
+		return
+	}
+
+	grip.Error(srv.Run(ctx))
 }
